9009de0d4d61aec1361a75039f7b3cad43d9115a
1111 Technologies <rpranaya.2000@gmail.com>
2021-06-23 20:45:38 +0800

    another main route test

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.get("/",(req,res, next)=>{
         // res.json({
         //     data : 'hi'
         // });
-        res.send('this whatsupp main world');
+        res.send('this hell main world');
 
     }catch(e){
         console.log(e)
