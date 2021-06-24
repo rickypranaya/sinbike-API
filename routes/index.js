fd7c5b50a8a24da13d59f8e96b59065dc458b5c4
1111 Technologies <rpranaya.2000@gmail.com>
2021-06-24 23:15:16 +0800

    delete all the test tix

--- routes/index.js
+++ routes/index.js
@@ -31,29 +31,4 @@ router.get("/",(req,res, next)=>{
     }
 });
 
-
-router.get("/tix-3",(req,res, next)=>{
-    const params = req.body;
-
-    try{
-        // let results = await db.users_add(params);
-         res.json({
-             data : 'tix-3'
-         });
-router.get("/this is tix-2", async (req,res, next)=>{
-    const params = req.body;
-
-    try{
-       // let results = await db.users_add(params);
-        res.json({
-            data : "hello"
-        });
-
-    }catch(e){
-        console.log(e)
-        res.sendStatus(500);
-    }
-});
-
-
 module.exports = router;
