2017f5c02322214d43db15add94284cb49e3b7c0
1111 Technologies <rpranaya.2000@gmail.com>
2021-08-13 16:49:20 +0800

    as

--- routes/index.js
+++ routes/index.js
@@ -274,6 +274,7 @@ router.post("/get_reserve", async (req,res, next)=>{
 
             var min10 = moment().subtract(10, 'minutes');
             var dbDate = results[0].created_at;
+            var cleanDate = dbDate.replace('Z', ' ').replace('T', ' ')
             // var database = moment(dbDate.replace('Z', ' ').replace('T', ' '))
 
             // if(min10 <= database){
@@ -287,7 +288,7 @@ router.post("/get_reserve", async (req,res, next)=>{
                     // let deleteQ = await db.reserve_delete(results[0].id);
                     res.json({
                         status : 200,
-                        data : dbDate,
+                        data : cleanDate,
                         message : 'reservation expired'
                     });
                     // res.sendStatus(200);
@@ -302,7 +303,7 @@ router.post("/get_reserve", async (req,res, next)=>{
         
     }catch(e){
         console.log(e)
-        res.status(506).send(e);
+        res.status(507).send(e);
     }
 });
 
